d84d5d356377fcc348486e12ba32a24a6ec9da80
Cheska Huang <julyannahuang@gmail.com>
2026-05-26 20:11:04 +0800

    feature: navbar header, links for footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,103 @@
 import type { Metadata } from "next";
 import { MenuIcon, SearchIcon } from "lucide-react";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Chivo, Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const tiemposHeadline = localFont({
+  src: [
+    // Light (300)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-LIGHT.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-LIGHTITALIC.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    // Regular (400)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-REGULAR.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-REGULARITALIC.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    // Medium (500)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-MEDIUM.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-MEDIUMITALIC.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    // SemiBold (600)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-SEMIBOLD.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-SEMIBOLDITALIC.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    // Bold (700)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-BOLD.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-BOLDITALIC.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    // Black (900)
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-BLACK.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/tiempos-headline/TIEMPOSHEADLINE-BLACKITALIC.woff2",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-tiempo-headline",
+  display: "swap",
+});
+
+const chivo = Chivo({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-chivo",
+  display: "swap",
+});
+
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" });
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +115,11 @@ export const metadata: Metadata = {
     "The GUIDON is the official student publication of the Ateneo de Manila University. © 2026 The GUIDON All Rights Reserved",
 };
 
+const menuNoHighlight = cn(
+  navigationMenuTriggerStyle(),
+  `hover:bg-transparent! focus:bg-transparent! bg-transparent! ${chivo.className}`,
+);
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -38,10 +135,13 @@ export default function RootLayout({
         geistMono.variable,
         "font-sans",
         inter.variable,
+        chivo.variable,
+        tiemposHeadline.variable,
+        roboto.variable,
       )}
     >
       <body className="min-h-full flex flex-col">
-        <header className="w-full h-[116px] py-4 px-12 bg-[#1c4480] text-white">
+        <header className="w-full h-[116px] py-4 px-12 bg-[#1c4480] text-white flex flex-col items-center font-chivo">
           <section className="w-full flex flex-nowrap justify-between items-center h-min">
             <MenuIcon />
             <Image
@@ -52,6 +152,83 @@ export default function RootLayout({
             />
             <SearchIcon />
           </section>
+          <NavigationMenu className="mt-2">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/" className={menuNoHighlight}>
+                  Home
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className={menuNoHighlight}>
+                  Sections
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="w-40">
+                    {[
+                      "News",
+                      "Opinion",
+                      "Sports",
+                      "Beyond Loyola",
+                      "Features",
+                      "Inquiry",
+                      "SciTech",
+                    ].map((item) => (
+                      <li key={item}>
+                        <NavigationMenuLink
+                          href={`/sections/${item.toLowerCase().replace(/\s+/g, "-")}`}
+                          className={menuNoHighlight}
+                        >
+                          {item}
+                        </NavigationMenuLink>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className={menuNoHighlight}>
+                  Specials
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="w-48">
+                    {[
+                      "Vantage Magazine",
+                      "Multimedia",
+                      "Interactives",
+                      "Archive",
+                    ].map((item) => (
+                      <li key={item}>
+                        <NavigationMenuLink
+                          href={`/specials/${item.toLowerCase().replace(/\s+/g, "-")}`}
+                          className={menuNoHighlight}
+                        >
+                          {item}
+                        </NavigationMenuLink>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/take-the-lede"
+                  className={menuNoHighlight}
+                >
+                  Take the Lede
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/about" className={menuNoHighlight}>
+                  About
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </header>
         {children}
         <footer className="w-full grid grid-cols-3 grid-rows-1 justify-items-center gap-16 pb-8 pt-12 px-12 bg-[#1c4480] text-white">
@@ -77,48 +254,72 @@ export default function RootLayout({
           <section className="flex flex-nowrap gap-8">
             <ul>
               <li>
-                <p>Browse</p>
+                <p className="font-bold">Browse</p>
               </li>
               <li>
-                <p>Beyond Loyola</p>
+                <a href="/beyond-loyola">
+                  <p>Beyond Loyola</p>
+                </a>
               </li>
               <li>
-                <p>Opinion</p>
+                <a href="/opinion">
+                  <p>Opinion</p>
+                </a>
               </li>
               <li>
-                <p>Sports</p>
+                <a href="/sports">
+                  <p>Sports</p>
+                </a>
               </li>
               <li>
-                <p>Features</p>
+                <a href="/features">
+                  <p>Features</p>
+                </a>
               </li>
               <li>
-                <p>Inquiry</p>
+                <a href="/inquiry">
+                  <p>Inquiry</p>
+                </a>
               </li>
               <li>
-                <p>SciTech</p>
+                <a href="/scitech">
+                  <p>SciTech</p>
+                </a>
               </li>
             </ul>
             <ul>
               <li>
-                <p>More</p>
+                <p className="font-bold">More</p>
               </li>
               <li>
-                <p>Vantage Magazine</p>
+                <a href="/vantage-magazine">
+                  <p>Vantage Magazine</p>
+                </a>
               </li>
               <li>
-                <p>Multimedia</p>
+                <a href="/multimedia">
+                  <p>Multimedia</p>
+                </a>
               </li>
               <li>
-                <p>Interactives</p>
+                <a href="/interactives">
+                  <p>Interactives</p>
+                </a>
               </li>
               <li>
-                <p>Archives</p>
+                <a href="/archives">
+                  <p>Archives</p>
+                </a>
               </li>
               <li>
-                <p>Take the Lede</p>
+                <a href="/take-the-lede">
+                  <p>Take the Lede</p>
+                </a>
               </li>
               <li>
-                <p>About</p>
+                <a href="/about">
+                  <p>About</p>
+                </a>
               </li>
             </ul>
           </section>
