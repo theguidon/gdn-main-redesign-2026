4112dd2b6ccf931071ade22ab116de5252ac9c73
Cheska Huang <julyannahuang@gmail.com>
2026-05-27 18:08:33 +0800

    feat: util function to format dates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -27,4 +28,8 @@ export function bylinesToP(authors: string[]) {
       wrapAuthorInSpans(authors[authors.length - 1])
     );
   }
+}
+
+export function formatDate(date: Date) {
+  return dayjs(date).format("MMMM D YYYY, H:mm A");
 }
