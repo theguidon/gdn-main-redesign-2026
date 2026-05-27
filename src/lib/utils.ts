57d6d2e6e08a86eec03e2c34608eb84f71f618f0
Cheska Huang <julyannahuang@gmail.com>
2026-05-27 17:54:37 +0800

    feat: utility function to create formatted bylines

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+function wrapAuthorInSpans(author: string) {
+  return "<span class='font-bold text-[#1c4480]'>" + author + "</span>";
+}
+
+export function bylinesToP(authors: string[]) {
+  if (authors.length == 0) return "";
+  if (authors.length == 1)
+    return "<p>By " + wrapAuthorInSpans(authors[0]) + "</p>";
+  if (authors.length == 2)
+    return (
+      "<p>By " +
+      wrapAuthorInSpans(authors[0]) +
+      " and " +
+      wrapAuthorInSpans(authors[1]) +
+      "</p>"
+    );
+  else {
+    return (
+      "<p>By " +
+      authors.slice(0, -1).map(wrapAuthorInSpans).join(", ") +
+      ", and " +
+      wrapAuthorInSpans(authors[authors.length - 1]) +
+      "</p>"
+    );
+  }
+}
