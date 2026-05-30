3063d47bcac989a43b2e40837da97e9678500c56
Cheska Huang <julyannahuang@gmail.com>
2026-05-30 17:18:02 +0800

    feat: home page with featured article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,59 @@
 import Image from "next/image";
+import { BASE_URL } from "@/lib/constants";
+import {
+  WPResponseToArticle,
+  bylinesToP,
+  formatDate,
+  chipFromCategory,
+} from "@/lib/utils";
+import { Article, WPResponse } from "@/lib/types";
 
-export default function Home() {
-  const BASE_URL = "https://theguidon.com/wp-json/wp/v2/posts";
-  const latest_articles = fetch(`${BASE_URL}?per_page=4`).then((res) =>
+export default async function Home() {
+  const fetchData = await fetch(`${BASE_URL}/posts?per_page=4`).then((res) =>
     res.json(),
   );
-  return <main className="flex flexcol items-stretch px-1/5 py-10"></main>;
+  const latestArticles: Article[] = (fetchData as WPResponse[]).map(
+    WPResponseToArticle,
+  );
+
+  const featuredArticle = latestArticles[0];
+
+  return (
+    <main className="flex flex-col items-stretch px-[5vw] py-10 min-h-screen">
+      <section className="flex flex-row items-stretch gap-0 h-[500px] bg-(--off-white)">
+        <Image
+          src={featuredArticle.featured_image_url}
+          alt={featuredArticle.title}
+          width={800}
+          height={700}
+          className={"object-cover grow"}
+        />
+        <section className={"w-2/5 px-10 py-8"}>
+          <section className="flex flex-row gap-2 mt-10 mb-4">
+            {featuredArticle.categories.map((catID: number) => (
+              <div key={catID}>{chipFromCategory(catID)}</div>
+            ))}
+          </section>
+          <h1 className={"font-tiempos-headline text-4xl font-bold"}>
+            {featuredArticle.title}
+          </h1>
+          <section className={"my-3 text-lg"}>
+            <p
+              className="p-0 m-0 uppercase"
+              dangerouslySetInnerHTML={{
+                __html: bylinesToP(featuredArticle.authors),
+              }}
+            />
+            <p
+              className="p-0 m-0 text-[#4c4c4e] uppercase"
+              dangerouslySetInnerHTML={{
+                __html: formatDate(new Date(featuredArticle.pubDate)),
+              }}
+            />
+          </section>
+          <p className="text-lg">{featuredArticle.excerpt || ""}</p>
+        </section>
+      </section>
+    </main>
+  );
 }
